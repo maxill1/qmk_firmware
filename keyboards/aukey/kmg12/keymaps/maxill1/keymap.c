68eb77cf4a077cc265cd156d78bbd933535de460
Maxill1 <setmaat@gmail.com>
2022-02-01 17:29:09 +0100

    openrgb & polling

---
+++ keyboards/aukey/kmg12/keymaps/maxill1/keymap.c
@@ -0,0 +1,6 @@
+
+#include "../default/keymap.c"
+
+/* Change USB Polling Rate to 1000hz and a larger keys per scan for elite gaming*/
+#define USB_POLLING_INTERVAL_MS 1
+#define QMK_KEYS_PER_SCAN 12
